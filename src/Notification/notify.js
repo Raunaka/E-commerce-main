8226fd8cb26a29a0944793c1883b53e9d9100271
Raunak kumar singh <k.rounak6@gmail.com>
2024-04-13 01:00:45 +0530

    Update notify.js

--- src/Notification/notify.js
+++ src/Notification/notify.js
@@ -3,6 +3,5 @@ import { toast } from "react-toastify";
 export const showToastMessage = (message, type) => {
   toast[type](message, {
     position: toast.POSITION.TOP_RIGHT,
-    onClose: () => {}, // Function to execute when the toast is closed
   });
 };
